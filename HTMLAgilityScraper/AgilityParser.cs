ba982e087086e83f33842bd87bafb2a20881b48f
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-10-28 18:16:03 -0500

    working on inserting my data in db.

--- HTMLAgilityScraper/AgilityParser.cs
+++ HTMLAgilityScraper/AgilityParser.cs
@@ -56,23 +56,23 @@ namespace HTMLAgilityScraper
         public void grabAndPrintStockToConsole()
         {
 
-        }
+            string googleFinance = "https://google.com/finance";
 
-        /*
-          
+
         List<ParseTable> ListOfStocks = new List<ParseTable>();
+
             foreach (var stock in stockTable)
             {
                 DateTime stockRecord = DateTime.Now;
 
-        string symbol = stock.SelectSingleNode("/html/body/div[4]/div/main/div/article/div[2]/div/section[2]/div/div/div[2]/div[4]/table/tbody/tr[1]/th").InnerText;
-        string company = stock.SelectSingleNode("/html/body/div[4]/div/main/div/article/div[2]/div/section[2]/div/div/div[2]/div[4]/table/tbody/tr[1]/td[1]").InnerText;
-        string lastSale = stock.SelectSingleNode("/html/body/div[4]/div/main/div/article/div[2]/div/section[2]/div/div/div[2]/div[4]/table/tbody/tr[1]/td[2]/a").InnerText;
-        string change = stock.SelectSingleNode("/html/body/div[4]/div/main/div/article/div[2]/div/section[2]/div/div/div[2]/div[4]/table/tbody/tr[1]/td[3]").InnerText;
-        string pChg = stock.SelectSingleNode("/html/body/div[4]/div/main/div/article/div[2]/div/section[2]/div/div/div[2]/div[4]/table/tbody/tr[1]/td[4]").InnerText;
+                string symbol = stock.SelectSingleNode("/html/body/div[4]/div/main/div/article/div[2]/div/section[2]/div/div/div[2]/div[4]/table/tbody/tr[1]/th").InnerText;
+                string company = stock.SelectSingleNode("/html/body/div[4]/div/main/div/article/div[2]/div/section[2]/div/div/div[2]/div[4]/table/tbody/tr[1]/td[1]").InnerText;
+                string lastSale = stock.SelectSingleNode("/html/body/div[4]/div/main/div/article/div[2]/div/section[2]/div/div/div[2]/div[4]/table/tbody/tr[1]/td[2]/a").InnerText;
+                string change = stock.SelectSingleNode("/html/body/div[4]/div/main/div/article/div[2]/div/section[2]/div/div/div[2]/div[4]/table/tbody/tr[1]/td[3]").InnerText;
+                string pChg = stock.SelectSingleNode("/html/body/div[4]/div/main/div/article/div[2]/div/section[2]/div/div/div[2]/div[4]/table/tbody/tr[1]/td[4]").InnerText;
 
-        ParseTable Stocks = new ParseTable();
-        Stocks.StockRecord = stockRecord;
+                ParseTable Stocks = new ParseTable();
+                Stocks.StockRecord = stockRecord;
                 Stocks.Symbol = symbol;
                 Stocks.Company = company;
                 Stocks.LastSale = lastSale;
@@ -81,6 +81,8 @@ namespace HTMLAgilityScraper
                 ListOfStocks.Add(Stocks);
 
                 InsertDataToTable(Stocks);
-                */
+            }
+
+        }
     }
 }     
